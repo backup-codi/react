3fd6b735efa1aa2eb90beb79f9614ee2198a2c67
Alberto López <engineer.lopezx2@gmail.com>
2020-05-18 14:28:54 -0500

    creando nuestro primer componente

---
+++ src/components/Badge.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import confLogo from "../images/badge-header.svg";
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img src={confLogo} alt="Logo de la conferencia" />
+        </div>
+
+        <div>
+          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+          <h1>
+            Alberto
+            <br />
+            López
+          </h1>
+        </div>
+
+        <div>
+          <p>Frontend Engineer</p>
+          <p>@ing_alberto27</p>
+        </div>
+
+        <div>#platziConf</div>
+      </div>
+    );
+  }
+}
+
+export default Badge;
