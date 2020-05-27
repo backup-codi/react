3de04cbb2af51a97b177f08ced8ed2f965f4023e
Alberto López <engineer.lopezx2@gmail.com>
2020-05-26 19:39:17 -0500

    Manejo de estados

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  // Inicializamos el estado para cuando cargue la pagina no marque error el value de los input
+  state = {
+    jobTitle: "Desinger",
+  };
   handleChange = (e) => {
-    // console.log({ e }); // Imprime el evento
-    //si queremos ver el valor de ese evento
-    console.log({
-      name: e.target.name,
-      value: e.target.value,
+    // // console.log({ e }); // Imprime el evento
+    // //si queremos ver el valor de ese evento
+    // console.log({
+    //   name: e.target.name,
+    //   value: e.target.value,
+    // });
+    this.setState({
+      // Dependiendo el input que se llame se guardara con el nombre de esa llave
+      [e.target.name]: e.target.value,
     });
   };
   handleClick = (e) => {
@@ -15,6 +23,7 @@ class BadgeForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form was submitted");
+    console.log(this.state);
   };
 
   render() {
@@ -25,16 +34,57 @@ class BadgeForm extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label>Firs Name:</label>
-            {/* En un input cada que escribimos react nos da un evento onChange */}
             <input
               onChange={this.handleChange}
               className="form-control"
               type="text"
               name="firstName"
+              value={this.state.fistName}
             />
           </div>
-          {/* El boton tiene un evento onClick */}
-          {/* Por default button tiene el atributo type="submit" lo cual hace recargar la pagina. para omitir esta accion lo cambiamos a button. La otra opcion es desde form con onSubmit*/}
+          <div className="form-group">
+            <label>Last Name:</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email:</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="email"
+              name="email"
+              value={this.state.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job Title:</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              value={this.state.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Twitter:</label>
+            <input
+              onChange={this.handleChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              value={this.state.twitter}
+            />
+          </div>
           <button onClick={this.handleClick} className="btn btn-primary">
             Save
           </button>
