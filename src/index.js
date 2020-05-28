9c55b381c9aeec2ed88d234d16ea4dcb6b588b77
Alberto López <engineer.lopezx2@gmail.com>
2020-05-28 17:07:52 -0500

    División de laaplicación en rutas

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 import "./global.css";
-import BadgeNew from "./pages/BadgeNew";
-import Badges from "./pages/Badges";
+import App from "./components/App";
 
 const contailer = document.getElementById("app");
 
-ReactDOM.render(<Badges />, contailer);
+ReactDOM.render(<App />, contailer);
